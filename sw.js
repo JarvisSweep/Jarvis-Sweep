92004e7403f128a160ff8e45f8977e0a540aa3db
JarvisSweep <geetakerisk@gmail.com>
2026-04-08 05:27:31 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'jarvis-v1';
+const urlsToCache = [
+  '/',
+  '/jarvis_journal.html',
+  '/jarvis_assistant.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
